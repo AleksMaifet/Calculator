d6190371e368c72d4f554e81af901e0fec891968
AleksMaifet <aleksmaifet@gmail.com>
2022-10-13 09:56:12 +0300

    made changes in server and webpack config

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
-app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
-app.listen(port)
+app.listen(port);
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
     host: 'localhost',
     port: 3030,
